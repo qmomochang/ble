efb0ac2c15259563c90ce1109f02ab031ed86524
michael_chang <michael_chang@htc.com>
2015-06-30 14:01:59 +0800

    [init]

---
+++ src/main/java/com/htc/blelib/BleLib.java
@@ -0,0 +1,10 @@
+package com.htc.blelib;
+
+import android.util.Log;
+
+public class BleLib {
+    private static final String TAG="BleLib";
+    public void BleLib() {
+        Log.v(TAG,"init");
+    }
+}
